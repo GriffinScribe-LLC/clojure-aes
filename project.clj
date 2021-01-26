b91ebfee044a44ab88124560c6d224682942b8e9
Scott Griffin <scott.griffin1700@gmail.com>
2021-01-25 20:31:58 -0700

    Updated url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-aes "0.1.1"
   :description "Pure Clojure implementation of the Advanced Encryption Standard (AES). Supports encryption and decryption for 128, 192, and 256 bit keys."
-  :url "https://www.griffinscribe.com"
+  :url "https://github.com/GriffinScribe-LLC/clojure-aes"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"	
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
